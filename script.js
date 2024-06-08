8cfbcec42f2a5e65084f648d82e095fceb357f37
Shahin_Rana <147462060+shahinrana77@users.noreply.github.com>
2024-06-08 16:31:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let nabToggle = document.querySelector(".navigations");
+let menuBtn = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", ()=>{
+  nabToggle.classList.toggle("activeNav");
+})
